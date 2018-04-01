8da8b78f778bb9f0815b7b86d7de97519d61d148
Elansary Mahmoud <mahmoud.elansary2012@gmail.com>
2018-04-01 23:25:03 +0200

    version_2

--- Dir-Stat_program.py
+++ Dir-Stat_program.py
@@ -6,26 +6,42 @@ import plotly.graph_objs as go
 import numpy as np
 import os
 import sys
+from optparse import OptionParser
 
-print('Number of arguments:', len(sys.argv), 'arguments.')
-print('Argument List:', str(sys.argv))
+parser = OptionParser()
+parser.add_option("-n", "--num", dest="size",default=10
+                  ,help="Check the size of N directories", metavar="FILE")
+parser.add_option("-o", "--out", dest="filename",default='TMP_size.txt',
+                  help="write report to FILE", metavar="FILE")
+parser.add_option("-q", "--quiet",
+                  action="store_false", dest="verbose", default=True,
+                  help="don't print status messages to stdout")
 
-size = int(sys.argv[1])
-os.system('du -sk ./*/ | sort -n > tmp_size.txt')
-df = pd.read_csv("tmp_size.txt",sep="\t",header=None,names=['size','directory'])
+(options, args) = parser.parse_args()
+
+#print('Number of arguments:', len(sys.argv), 'arguments.')
+#print('Argument List:', str(sys.argv))
+#size = int(sys.argv[1])
+
+print("arguments:", args)
+print("options:", options)
+print(str(options.filename))
+size = int(options.size)
+filename = str(options.filename)
+os.system('du -sk ./*/ | sort -n >' + filename)
+df = pd.read_csv(filename,sep="\t",header=None,names=['size','directory'])
 df = df.sort_values('size',ascending=False)
 values = df['size'].apply(lambda x: np.array(x))
 labels = df['directory'].apply(lambda x: np.array(x))
 new_values = values[0:size].tolist()
-new_values.append(df[(size+1):]['size'].sum())
+new_values.append(df[(size + 1):]['size'].sum())
 new_labels = labels[0:size].tolist()
 new_labels.append('./others/')
 new_labels = [ i.split('/')[1] for i in new_labels ]
 new_labels = [ i[:40] + "..." if len(i) > 40 else i  for i in new_labels ]
-new_labels = [ new_labels[i] + " (" + str(round(new_values[i] / 1024000,2)) + "G)" for i in range(0,(size+1))  ]
+new_labels = [ new_labels[i] + " (" + str(round(new_values[i] / 1024000,2)) + "G)" for i in range(0,(size + 1))  ]
 
 layout = go.Layout(height = 600,width = 600, autosize = False,title = "Total Directory size " + str(round(df['size'].sum()/1024000,2)) + "G")
 trace = go.Pie(labels=new_labels,values=new_values,hoverinfo='label+percent')
 fig = go.Figure(data = [trace], layout = layout)
 offline.plot(fig, auto_open=True, image = 'png', image_filename='Directory_size',output_type='file', image_width=800, image_height=600, validate=False)
-
